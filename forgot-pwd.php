ce485ef0bc5d877783e110a88307991295c2eb66
[OnyangoKabarak] <[sodipo@kabarak.ac.ke]>
2024-02-05 20:29:13 +0300

    Changes

--- forgot-pwd.php
+++ forgot-pwd.php
@@ -5,7 +5,7 @@ require "db.php";
     <h1>Reset Your Password</h1>
     <p>An email with the reset link has been sent to you.</p>
     <form action="includes/reset-request.php" method="post">
-        <input type="text" name="email" placeholder="Enter Your Email Address">
+        <input type="text" name="email" placeholder="Enter Your Email Your Address">
         <button type="submit" name="reset-request-submit">Recover Password</button>
     </form>
 </main>
