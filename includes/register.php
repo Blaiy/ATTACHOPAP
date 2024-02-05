b82dcdbe8f084ec6201c6e2e93dd5247295f1570
[OnyangoKabarak] <[sodipo@kabarak.ac.ke]>
2024-02-05 22:07:45 +0300

    Current changes

--- includes/register.php
+++ includes/register.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 include '../db.php';
-=======
-include 'db.php';
->>>>>>> a88fea83ac0e8e13f6a29ca10512afe18e517784
 
 // Check the connection
 if ($conn->connect_error) {
@@ -12,7 +8,6 @@ if ($conn->connect_error) {
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-<<<<<<< HEAD
     $fullname = isset($_POST["fullname"]) ? $_POST["fullname"] : '';
     $email = isset($_POST["email"]) ? $_POST["email"] : '';
     $password = isset($_POST["password"]) ? $_POST["password"] : '';
@@ -21,16 +16,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Insert data into the database
     $sql = "INSERT INTO students (fullname, email, password, course, institution) VALUES ('$fullname', '$email', '$password', '$course', '$institution')";
-=======
-    $fullname = $_POST["fullname"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $course = $_POST["course"];
-    $institution = $_POST["institution"];
-
-    // Insert data into the database
-    $sql = "INSERT INTO users (fullname, email, password, course, institution) VALUES ('$fullname', '$email', '$password', '$course', '$institution')";
->>>>>>> a88fea83ac0e8e13f6a29ca10512afe18e517784
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
@@ -42,7 +27,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Close the database connection
 $conn->close();
 ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> a88fea83ac0e8e13f6a29ca10512afe18e517784
